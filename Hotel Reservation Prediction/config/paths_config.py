9d76f703e041a9d02999917889d185ecc6aeb5c4
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-09-06 13:06:05 +0300

    configuration paths

---
+++ Hotel Reservation Prediction/config/paths_config.py
@@ -0,0 +1,8 @@
+import os
+
+RAW_DIR = "Hotel Reservation Prediction/artifacts/raw"
+RAW_FILE_PATH = os.path.join(RAW_DIR, "raw.csv")
+TRAIN_FILE_PATH = os.path.join(RAW_DIR, "train.csv")
+TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
+
+CONFIG_PATH = "Hotel Reservation Prediction/config/config.yaml"
