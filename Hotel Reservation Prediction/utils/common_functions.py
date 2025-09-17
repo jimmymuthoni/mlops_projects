9b167f381552cb218ecf65b88133518184c05983
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-09-17 18:52:41 +0300

    function to load data implemented

--- Hotel Reservation Prediction/utils/common_functions.py
+++ Hotel Reservation Prediction/utils/common_functions.py
@@ -2,6 +2,7 @@ import os
 from src.logger import get_logger
 from src.custom_exception import CustomException
 import yaml
+import pandas as pd
 
 logger = get_logger(__name__)
 
@@ -16,3 +17,12 @@ def read_yaml(file_path):
     except Exception as e:
         logger.error("Error while reading YAML file")
         raise CustomException("Failed to read YAML file", e)
+    
+
+def load_data(path):
+    try:
+        logger.info("**Loading data**")
+        return pd.read_csv(path)
+    except Exception as e:
+        logger.error(f"Error while loading the data", {e})
+        raise CustomException("Failed to load data")
